6d136fe5c855009b2328278cc7dfd2a94324e323
Vadim <vadimyatsunyk@gmail.com>
2023-03-31 03:25:23 +0300

    325

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var i = 0;
-var txt = "Great stuff! Cant wait to check out the Try it Yourself!";
+var txt =
+  "Saving time, Personalization, Improving user experience, Better outcomes.With well-designed prompts, customers can complete tasks or get the information they need more quickly and efficiently. This can be especially valuable in customer service scenarios, where customers may be looking for quick answers or resolutions to their problems";
 var speed = 50;
 
 function typeWriter() {
